abfbf1345d4013386281db276b5f49ed8fb9004f
Cardosok <1744965@TCS.com>
2019-09-12 14:10:46 -0300

    criando os principais components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
+import { InvestmentComponent } from './components/investment/investment.component';
+import { LoanComponent } from './components/loan/loan.component';
+import { TransferComponent } from './components/transfer/transfer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent,
+    InvestmentComponent,
+    LoanComponent,
+    TransferComponent
   ],
   imports: [
     BrowserModule
