2b99365a91ece06a3452cc85a8cf00bcf22a9975
1744965 <cesarcardoso954@gmail.com>
2019-09-16 12:10:39 -0300

    rota login

--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { UserService } from '../../services/users/user.service';
+import { User } from '../../models/users/user';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -16,15 +18,20 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./user-registration.component.css']
 })
 export class UserRegistrationComponent implements OnInit {
+  private user: User;
   nomeFormControl = new FormControl('', [
     Validators.required,
     Validators.maxLength(55),
     Validators.pattern(/^[a-zA-Z\s]*$/)
   ]);
   matcher = new MyErrorStateMatcher();
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  login(user) {
+    this.userService.login(user);
   }
 
 }
