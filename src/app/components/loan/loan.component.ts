58832fbe0c4f354a839e8b5c421dfdf00c02170a
Samuel Silva <1745035@TCS.com>
2019-09-17 17:02:57 -0300

    PONTO DA MALDIÇÃO

--- src/app/components/loan/loan.component.ts
+++ src/app/components/loan/loan.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,4 +16,4 @@ export class LoanComponent implements OnInit {
     console.log('clique');
     this.router.navigate(['index']);
   }
-}
+}  
