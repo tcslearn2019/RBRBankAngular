2b1ede7f5fa81dc07a6b475bab61de968626b837
1744965 <cesarcardoso954@gmail.com>
2019-09-18 15:44:40 -0300

    populando as telas

--- src/app/components/transfer/transfer.component.ts
+++ src/app/components/transfer/transfer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/users/user';
+import { UserService } from 'src/app/services/users/user.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { TransferRequest } from 'src/app/request/transfer-request';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-transfer',
@@ -6,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transfer.component.css']
 })
 export class TransferComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  transf = new FormGroup({
+    accountReciever: new FormControl(),
+    value: new FormControl(),
+    password: new FormControl()
+ });
+
+  constructor(private userservice: UserService, private router: Router) { }
 
   ngOnInit() {
+    this.user = this.userservice.getterUser();
   }
+
+doTransfer(transf) {
+  if (transf.value.password === this.user.password) {
+    const transfFormatado = this.formatTransfer(transf.value);
+    this.userservice.doTransfer(transfFormatado).subscribe(r => {
+      console.log(r);
+      this.router.navigate(['index']);
+    },
+    err => {
+      console.log('errs');
+      console.log(err);
+    });
+  }
+}
+
+cancelar() {
+  this.router.navigate(['index']);
+}
+
+formatTransfer(transf): TransferRequest {
+  const transfReq = new TransferRequest();
+  transfReq.receiverId = transf.accountReciever;
+  transfReq.senderId = this.user.account.numberAccount;
+  transfReq.value = transf.value;
+
+  return transfReq;
+}
+
+
 }
---
+++ src/app/request/transfer-request.ts
@@ -0,0 +1,5 @@
+export class TransferRequest {
+    senderId: number;
+    receiverId: number;
+    value: number;
+}
