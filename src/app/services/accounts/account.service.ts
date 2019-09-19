2fb889fed0d1540aad02b7aea32e44ea1201f380
1744965 <cesarcardoso954@gmail.com>
2019-09-19 14:47:25 -0300

    pull

--- src/app/services/accounts/account.service.ts
+++ src/app/services/accounts/account.service.ts
@@ -6,7 +6,6 @@ import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
 })
 export class AccountService {
   private baseUrl: string = 'http://localhost:8080/rbr/account';
-  
   private parans: HttpParams;
   private headers: HttpHeaders;
   constructor(private http: HttpClient) { }
