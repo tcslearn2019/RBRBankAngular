c48188a82e51abc70bb22a5563c4f1370a0492cb
Taynara Carolini Almeida <1745037@TCS.com>
2019-09-19 11:04:51 -0300

    pull

--- src/app/components/investment/investment.component.ts
+++ src/app/components/investment/investment.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CheckboxControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-investment',
