0b3d2d5c9873465db5deaca9e915719731ee5f79
Taynara Carolini Almeida <1745037@TCS.com>
2019-09-20 09:46:24 -0300

    outro commit

--- src/app/components/investment-cdi/investment-cdi.component.ts
+++ src/app/components/investment-cdi/investment-cdi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/users/user';
 
 @Component({
   selector: 'app-investment-cdi',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./investment-cdi.component.css']
 })
 export class InvestmentCdiComponent implements OnInit {
-
+  user:User;
   constructor() { }
 
   ngOnInit() {
