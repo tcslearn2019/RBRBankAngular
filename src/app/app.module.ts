8fcfce8c43a5639468bff4263c3dea08f40cb0b7
Taynara Carolini Almeida <1745037@TCS.com>
2019-09-20 09:24:15 -0300

    telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import {MatDividerModule} from '@angular/material/divider';
 import {HttpClientModule} from '@angular/common/http';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 const appRoutes: Routes = [
   { path: '', component: UserRegistrationComponent },  
@@ -91,7 +92,8 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
     MatListModule,
     MatRadioModule,
     MatSelectModule,
-    MatDialogModule 
+    MatDialogModule,
+    MatTooltipModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent],
